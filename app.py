b241bfbfa22942c8d95bc97fb0098d77e97fcd74
mxs <mxs@sbrk.org>
2021-04-29 21:45:25 +0200

    feat: import flask app

---
+++ app.py
@@ -0,0 +1,18 @@
+from sandwalker import create_app
+from sandwalker import fixtures, models
+from ilock import ILock
+
+
+def make_app():
+    with ILock('sandwalker-init-lock'):
+        app = create_app()
+        with app.app_context():
+            fixtures.init_app(app, models.db)
+    return app
+
+
+app = make_app()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
