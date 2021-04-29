e7221057a09a2543768e9f2b35ca5b4c0508d6f7
mxs <mxs@sbrk.org>
2021-04-29 22:58:58 +0200

    feat: add unit tests

---
+++ tests.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+ 
+from sandwalker import create_app
+from sandwalker import models, routes
+
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        with self.app.app_context():
+            self.test_app = self.app.test_client()
+ 
+    def tearDown(self):
+        with self.app.app_context():
+            models.db.session.close()
+            models.db.drop_all()
+
+    def test_home(self):
+        response = self.test_app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        
+ 
+if __name__ == "__main__":
+    unittest.main()
