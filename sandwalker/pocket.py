d5f3886a23df85aff5c1d427c6f8f371e14aff6e
mxs <mxs@sbrk.org>
2021-05-02 20:05:45 +0200

    feat: add block-by-month Pocket data

---
+++ sandwalker/pocket.py
@@ -0,0 +1,21 @@
+"""Sandwalker pocket data"""
+
+# This is temporary for now, and to be moved to the database at some
+# point. Alternatively, we could also store the timestamp of the block
+# in the database.
+#
+# This was computed using https://github.com/aimxhaisse/pocket_counter_experiments
+
+BLOCK_DATES = {
+    '2020-07-01': [None, 324],
+    '2020-08-01': [325, 3294],
+    '2020-09-01': [3295, 6121],
+    '2020-10-01': [6122, 9009],
+    '2020-11-01': [9010, 11795],
+    '2020-12-01': [11796, 14690],
+    '2021-01-01': [14691, 17379],
+    '2021-02-01': [17380, 19704],
+    '2021-03-01': [19705, 22182],
+    '2021-04-01': [22183, 24740],
+    '2021-05-01': [24741, None],
+}
